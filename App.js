2b94caf9e02ee4cddb20115e7b3ecaaba14a354b
RiQhy <riku.nokelainen@metropolia.fi>
2023-08-22 17:12:23 +0300

    minor fix

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 /* eslint-disable react/display-name */
 import {StatusBar} from 'expo-status-bar';
-import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  View,
+} from 'react-native';
 
 const mediaArray = [
   {
@@ -37,23 +45,30 @@ const mediaArray = [
 
 const App = () => {
   return (
-    <FlatList
-    data={mediaArray}
-    renderItem={({item}) => {
-      return (
-        <TouchableOpacity>
-          <Image
-            style={{width: 100, height: 100}}
-            source={{uri: item.thumbnails.w160}}
-          />
-          <View>
-            <Text>{item.title}</Text>
-            <Text>{item.description}</Text>
-          </View>
-        </TouchableOpacity>
-      );
-    }}
-  />
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={mediaArray}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                console.log('touched!', item.title);
+              }}
+            >
+              <Image
+                style={{width: 100, height: 100}}
+                source={{uri: item.thumbnails.w160}}
+              />
+              <Text>{item.title}</Text>
+              <Text>{item.description}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
