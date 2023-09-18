f3597610e576d322afb4a68373c8465acbab9002
RiQhy <riku.nokelainen@metropolia.fi>
2023-09-18 09:34:54 +0300

    minor changes

--- utils/appConfig.js
+++ utils/appConfig.js
@@ -1,6 +1,6 @@
 const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
 const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
 const placeholderImage = 'https://place-hold.it/300x200&text=Choose';
-const appId = 'hfdsjklah';
+const appId = 'wdfghjkolkmnb';
 
 export {apiUrl, mediaUrl, placeholderImage, appId};
